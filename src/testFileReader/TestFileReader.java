9b4d0d1a34e60fda90f473ac872c2bee6cc3b3a3
Niek van Dam <nvandam1@student.scalda.nl>
2018-03-04 17:21:54 +0100

    added better sysout

--- src/testFileReader/TestFileReader.java
+++ src/testFileReader/TestFileReader.java
@@ -11,7 +11,7 @@ public class TestFileReader {
 		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 	    fc.setAcceptAllFileFilterUsed(false);
 		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-			System.out.println("Current Directory" + fc.getCurrentDirectory());
+			return fc.getCurrentDirectory();
 		}
 	return "test";
 	}
