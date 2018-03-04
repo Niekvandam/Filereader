ee45731479b7dd3097dae35e2f69cd942d1e5be1
Niek van Dam <nvandam1@student.scalda.nl>
2018-03-04 17:21:01 +0100

    Able to select directory
    TestFileReader method SelectFolder now has a method that can start up the JFileReader and then returns the directory when okay is pressed

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+import testFileReader.TestFileReader;
+
 public class Main {
 
 	public static void main(String[] args) {
 	
+		TestFileReader t = new TestFileReader();
+		
+		t.SelectFolder();
 
 	}
 
--- src/testFileReader/TestFileReader.java
+++ src/testFileReader/TestFileReader.java
@@ -1,8 +1,18 @@
 package testFileReader;
 
+import javax.swing.JFileChooser;
+
 public class TestFileReader {
 
 	public String SelectFolder() {
-		return "test";
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new java.io.File("."));
+		fc.setDialogTitle("please select a folder");
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    fc.setAcceptAllFileFilterUsed(false);
+		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			System.out.println("Current Directory" + fc.getCurrentDirectory());
+		}
+	return "test";
 	}
 }
